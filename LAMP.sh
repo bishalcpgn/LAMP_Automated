611a13006210144ded97dda1bd310f699d390937
bishalcpgn <85829164+bishalcpgn@users.noreply.github.com>
2023-05-28 22:44:24 +0545

    Fully Automated

---
+++ LAMP.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Function to check installation status
+check_status() {
+    if [ $1 -eq 0 ]; then
+        echo -e "\n$2 installed successfully.\n"
+    else
+        echo -e "\nFailed to install $2.\n"
+        exit 1
+    fi
+}
+
+# To check root permissions
+if [ "$EUID" -ne 0 ]; then
+    echo -e "\nThis script must be run as root.\n" 
+    exit 1   
+else 
+    apt update && apt upgrade -y
+    check_status $? "Package updates"
+
+    apt install apache2 -y
+    check_status $? "Apache"
+
+    # systemctl status apache2
+
+    apt install mariadb-server -y
+    check_status $? "MariaDB"
+
+    # systemctl status mariadb
+
+    sudo mysql_secure_installation << input
+
+        y
+        root
+        root
+        y
+        y
+        y
+        y
+input
+    check_status $? "MYSQL_Secure_Installation"
+
+
+    apt install php -y
+    check_status $? "PHP"
+
+    
+    apt install phpmyadmin -y
+    check_status $? "phpMyAdmin"
+
+    echo -e "\nTask Completed Successfully.\n"
+fi
